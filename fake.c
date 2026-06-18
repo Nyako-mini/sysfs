95747e631e52e088e2ccf7633e6bbe6162e15738
Nyako_Desu <shuyue114514@gmail.com>
2026-06-18 20:43:30 +0800

    Update fake.c

--- fake.c
+++ fake.c
@@ -21,66 +21,75 @@
 #include <linux/err.h>
 #include <linux/slab.h>
 
-static int fake_enforcing = 0;
+static int fake_enforcing = -1;
 static struct kobject *selinux_fake_kobj;
 
 int get_fake_enforcing(void)
 {
-	if (fake_enforcing == 1)
-		return 1;
-	return -1;
+    if (fake_enforcing == 1)
+        return 1;
+    return -1;
 }
 EXPORT_SYMBOL(get_fake_enforcing);
 
-static ssize_t fake_show(struct kobject *kobj, struct kobj_attribute *attr,
-			 char *buf)
+void set_fake_enforcing(int val)
 {
-	return sprintf(buf, "%d\n", fake_enforcing);
+    if (val == 1)
+        fake_enforcing = 1;
+    else
+        fake_enforcing = -1;
+}
+EXPORT_SYMBOL(set_fake_enforcing);
+
+static ssize_t fake_show(struct kobject *kobj, struct kobj_attribute *attr,
+                         char *buf)
+{
+    return sprintf(buf, "%d\n", fake_enforcing);
 }
 
 static ssize_t fake_store(struct kobject *kobj, struct kobj_attribute *attr,
-			  const char *buf, size_t count)
+                          const char *buf, size_t count)
 {
-	int val;
-	int ret;
+    int val;
+    int ret;
 
-	ret = kstrtoint(buf, 10, &val);
-	if (ret)
-		return ret;
+    ret = kstrtoint(buf, 10, &val);
+    if (ret)
+        return ret;
 
-	if (val == 1) {
-		fake_enforcing = 1;
-		pr_info("Fake: enforcing detection set to 1 (fake)\n");
-	} else if (val == 0) {
-		fake_enforcing = 0;
-		pr_info("Fake: enforcing detection set to 0 (real)\n");
-	} else {
-		return -EINVAL;
-	}
+    if (val == 1) {
+        set_fake_enforcing(1);
+        pr_info("Fake: /sys/selinux/fake set to 1 (fake Enforcing)\n");
+    } else if (val == 0 || val == -1) {
+        set_fake_enforcing(0);
+        pr_info("Fake: /sys/selinux/fake set to real (fake disabled)\n");
+    } else {
+        return -EINVAL;
+    }
 
-	return count;
+    return count;
 }
 
 static struct kobj_attribute fake_attr = __ATTR(fake, 0644, fake_show, fake_store);
 
 static int __init fake_init(void)
 {
-	int ret;
+    int ret;
 
-	selinux_fake_kobj = kobject_create_and_add("selinux", kernel_kobj);
-	if (!selinux_fake_kobj) {
-		pr_err("Fake: failed to create selinux kobject\n");
-		return -ENOMEM;
-	}
+    selinux_fake_kobj = kobject_create_and_add("selinux", kernel_kobj);
+    if (!selinux_fake_kobj) {
+        pr_err("Fake: failed to create selinux kobject\n");
+        return -ENOMEM;
+    }
 
-	ret = sysfs_create_file(selinux_fake_kobj, &fake_attr.attr);
-	if (ret) {
-		pr_err("Fake: failed to create fake sysfs file\n");
-		kobject_put(selinux_fake_kobj);
-		return ret;
-	}
+    ret = sysfs_create_file(selinux_fake_kobj, &fake_attr.attr);
+    if (ret) {
+        pr_err("Fake: failed to create fake sysfs file\n");
+        kobject_put(selinux_fake_kobj);
+        return ret;
+    }
 
-	pr_info("Fake: /sys/selinux/fake created (0=real, 1=fake)\n");
-	return 0;
+    pr_info("Fake: /sys/selinux/fake created (write 1=fake, 0=real)\n");
+    return 0;
 }
 early_initcall(fake_init);
